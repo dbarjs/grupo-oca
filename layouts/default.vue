71f22bac370abe5fb0a66865a584ee60e24dc775
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-07 17:36:11 -0300

    Update logo and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,17 @@
     <v-app-bar app color="transparent" elevation="0">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <logo></logo>
+      <v-spacer></v-spacer>
+      <v-btn icon href="https://www.instagram.com/grupooca/" target="_blank">
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer app>
+    <v-footer app color="transparent justify-center">
       <span class="mr-1">Cavezzo</span>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
